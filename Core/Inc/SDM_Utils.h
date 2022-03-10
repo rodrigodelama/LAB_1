067f9162dbf469804c830b00ec3df0469ce306eb
Rodrigo De Lama <rodrigo.delama@icloud.com>
2022-03-10 11:37:52 +0100

    Included own libs
    SDM_Utils .h and .c

---
+++ Core/Inc/SDM_Utils.h
@@ -0,0 +1,4 @@
+
+void espera(int time);
+void Bin2Ascii(unsigned short number, unsigned char* chain);
+
---
+++ Core/Src/SDM_Utils.c
@@ -0,0 +1,22 @@
+void espera(int time)
+{
+  int i;
+  for (i = 0; i < time; i++);
+}
+
+void Bin2Ascii(unsigned short number, unsigned char* chain)
+{
+  unsigned short partial, j, cocient, divisor;
+
+  partial = number;
+  divisor = 10000;
+  *(chain) = ' ';
+  for (j = 1; j < 6; j++)
+  {
+    cocient = partial/divisor;
+    *(chain + j) = '0' + (unsigned char)cocient;
+    partial = partial - (cocient*divisor);
+    divisor = divisor/10;
+  }
+  *(chain + 6) = 0;
+}
